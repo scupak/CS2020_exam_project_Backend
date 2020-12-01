6c8f63b87ed320e6543b7e7f13dd5e378d1ce44a
kasp608b <54952896+kasp608b@users.noreply.github.com>
2020-12-01 21:10:41 +0100

    Added isAdmin boolen to Doctor

--- Core.Entities/Entities/BE/Doctor.cs
+++ Core.Entities/Entities/BE/Doctor.cs
@@ -7,6 +7,7 @@
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public string PhoneNumber { get; set; }
+        public bool IsAdmin { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
     }
