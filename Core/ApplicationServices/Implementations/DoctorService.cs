3b57b60653405aad45f24336333b1734a01b0f93
Lumby98 <lumby89@gmail.com>
2020-12-02 15:51:54 +0100

    set up the doctor service test

--- Core/ApplicationServices/Implementations/DoctorService.cs
+++ Core/ApplicationServices/Implementations/DoctorService.cs
@@ -2,16 +2,19 @@
 using Core.Entities.Entities.BE;
 using Core.Services.ApplicationServices.Interfaces;
 using Core.Services.DomainServices;
+using Core.Services.Validators.Interfaces;
 
 namespace Core.Services.ApplicationServices.Implementations
 {
     public class DoctorService : IService<Doctor, int>
     {
         private readonly IRepository<Doctor, int> _doctorRepository;
+        private readonly IDoctorValidator _doctorValidator;
 
-        public DoctorService(IRepository<Doctor, int> doctorRepository)
+        public DoctorService(IRepository<Doctor, int> doctorRepository, IDoctorValidator doctorValidator)
         {
             _doctorRepository = doctorRepository;
+            _doctorValidator = doctorValidator;
         }
 
         public List<Doctor> GetAll()
--- Infrastructure.UnitTests/ServiceTests/DoctorServiceTest.cs
+++ Infrastructure.UnitTests/ServiceTests/DoctorServiceTest.cs
@@ -1,20 +1,86 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Core.Entities.Entities.BE;
+using Core.Services.ApplicationServices.Implementations;
+using Core.Services.ApplicationServices.Interfaces;
 using Core.Services.DomainServices;
+using Core.Services.Validators.Implementations;
+using Core.Services.Validators.Interfaces;
+using FluentAssertions;
 using Moq;
+using Xunit;
 
 namespace Infrastructure.UnitTests.ServiceTests
 {
     public class DoctorServiceTest
     {
         private readonly Mock<IRepository<Doctor, int>> _doctorRepoMock;
+        private readonly Mock<IDoctorValidator> _doctorValidatorMock;
 
         private SortedDictionary<int, Doctor> _allDoctors;
 
-        public DoctorServiceTest(Mock<IRepository<Doctor, int>> doctorRepoMock, SortedDictionary<int, Doctor> allDoctors)
+        public DoctorServiceTest()
         {
-            _doctorRepoMock = doctorRepoMock;
-            _allDoctors = allDoctors;
+
+            _allDoctors = new SortedDictionary<int, Doctor>();
+            _doctorRepoMock = new Mock<IRepository<Doctor, int>>();
+            _doctorValidatorMock = new Mock<IDoctorValidator>();
+            
+            _doctorRepoMock
+                .Setup(repo => repo
+                    .Add(It.IsAny<Doctor>()))
+                .Callback<Doctor>(doctor => _allDoctors
+                    .Add(doctor.DoctorId, doctor))
+                .Returns<Doctor>(doctor => _allDoctors[doctor.DoctorId]);
+
+            _doctorRepoMock
+                .Setup(repo => repo
+                    .Edit(It.IsAny<Doctor>()))
+                .Callback<Doctor>(doctor => _allDoctors[doctor.DoctorId] = doctor)
+                .Returns<Doctor>(doctor => _allDoctors[doctor.DoctorId]);
+
+            _doctorRepoMock
+                .Setup(repo => repo
+                    .Remove(It
+                        .IsAny<int>()))
+                .Callback<int>(id => _allDoctors.Remove(id))
+                .Returns<Doctor>(doctor => _allDoctors[doctor.DoctorId]);
+
+            _doctorRepoMock
+                .Setup(repo => repo
+                    .GetAll())
+                .Returns(() => _allDoctors.Values.ToList());
+
+            _doctorRepoMock
+                .Setup(repo => repo
+                    .Get(It.IsAny<int>()))
+                .Returns<int>((id) => _allDoctors
+                    .ContainsKey(id) ? _allDoctors[id] : null);
         }
+
+        [Fact]
+        public void CreateDoctorService_ValidCompanyRepository()
+        {
+            // arrange
+            IRepository<Doctor, int> repo = _doctorRepoMock.Object;
+            IDoctorValidator validator = _doctorValidatorMock.Object;
+
+            // act
+            IService<Doctor, int> service = new DoctorService(repo, validator);
+
+            // assert
+            Assert.NotNull(service);
+        }
+
+        [Fact]
+        public void DoctorService_IsOfTypeIService()
+        {
+            
+            IRepository<Doctor, int> repo = _doctorRepoMock.Object;
+            IDoctorValidator validator = _doctorValidatorMock.Object;
+
+            new DoctorService(repo, validator).Should().BeAssignableTo<IService<Doctor, int>>();
+        }
+
     }
 }
