7a8982d57a4b84ea39e3c4fcce94266aed66ce14
scupak <anto1095@easv365.dk>
2020-12-03 19:35:21 +0100

    Update PatientsController.cs

--- UI.API/Controllers/PatientsController.cs
+++ UI.API/Controllers/PatientsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities.Entities.BE;
+using Core.Services.ApplicationServices.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,36 +14,78 @@ namespace UI.API.Controllers
     [ApiController]
     public class PatientsController : ControllerBase
     {
+        private IService<Patient, string> PatientService;
+
+        public PatientsController(IService<Patient, string> patientService)
+        {
+            PatientService = patientService;
+        }
+
         // GET: api/<PatientsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<Patient> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return Ok(PatientService.GetAll());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e);
+            }
         }
 
         // GET api/<PatientsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<Patient> Get(string id)
         {
-            return "value";
+            try
+            {
+
+                Patient patient = PatientService.GetById(id);
+                if (patient == null)
+                {
+                    return StatusCode(404, "patient not found");
+
+                }
+                return Ok(patient);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e);
+            }
         }
 
         // POST api/<PatientsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Patient>Post([FromBody] Patient patient)
         {
+            try
+            {
+                return string.IsNullOrEmpty(patient.PatientFirstName) ? BadRequest("FirstName is required to create a patient") : StatusCode(201, PatientService.Add(patient));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e);
+            }
+
         }
 
         // PUT api/<PatientsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult<Patient> Put(int id, [FromBody] Patient patient)
         {
+
         }
 
         // DELETE api/<PatientsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult<Patient> Delete(Patient patient)
         {
+
         }
     }
 }
