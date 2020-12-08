ac191e20806496520b96433ff221c560d9591e99
kasp608b <54952896+kasp608b@users.noreply.github.com>
2020-12-08 13:45:34 +0100

    Finished unit tests for appointments validator

--- Core/Validators/Implementations/AppointmentValidator.cs
+++ Core/Validators/Implementations/AppointmentValidator.cs
@@ -10,7 +10,29 @@ namespace Core.Services.Validators.Implementations
     {
         public void CreateValidation(Appointment appointment)
         {
-            throw new NotImplementedException();
+            if (appointment == null)
+            {
+                throw new NullReferenceException("Appointment cannot be null");
+            }
+
+            CreateIdValidation(appointment);
+            DateValidation(appointment);
+        }
+
+        private void DateValidation(Appointment appointment)
+        {
+            if (appointment.AppointmentDateTime == DateTime.Parse("0001 - 01 - 01T00: 00:00"))
+            {
+                throw new ArgumentException("an appointment needs a dateTime");
+            }
+        }
+
+        private void CreateIdValidation(Appointment appointment)
+        {
+            if (appointment.AppointmentId > 0)
+            {
+                throw new ArgumentException("A new appointment should not have an id");
+            }
         }
 
         public void EditValidation(Appointment appointment)
--- Infrastructure.UnitTests/ValidatorTests/AppointmentValidatorTest.cs
+++ Infrastructure.UnitTests/ValidatorTests/AppointmentValidatorTest.cs
@@ -53,7 +53,7 @@ namespace Infrastructure.UnitTests.ValidatorTests
                 DurationInMin = 15
             });
 
-            action.Should().Throw<ArgumentException>().WithMessage("An appointment should not have an id");
+            action.Should().Throw<ArgumentException>().WithMessage("A new appointment should not have an id");
         }
 
         [Fact]
@@ -121,7 +121,167 @@ namespace Infrastructure.UnitTests.ValidatorTests
                 Description = "my knee hurt",
                 DurationInMin = 0
             });
+            action.Should().Throw<ArgumentException>().WithMessage("The duration cannot be negative");
+        }
+
+        [Fact]
+        public void CreationValidation_AppointmentTooLongDuration_shouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.CreateValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(-1),
+                Description = "my knee hurt",
+                DurationInMin = 1441
+            });
+            action.Should().Throw<ArgumentException>().WithMessage("The duration cannot be longer than one day");
+        }
+
+        [Fact]
+        public void EditValidation_WithAppointmentThatsNull_shouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(null as Appointment);
+            action.Should().Throw<NullReferenceException>().WithMessage("Appointment cannot be null");
+        }
+
+        [Fact]
+        public void EditValidation_withInvalidAppointment_ShouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(1),
+                Description = "my knee hurt",
+                DurationInMin = 15
+            });
+
+            action.Should().Throw<ArgumentException>().WithMessage("When updating an appointment you need an id");
+        }
+
+
+        [Fact]
+        public void EditValidation_withAppointmentHasAnId_ShouldNotThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentId = 1,
+                AppointmentDateTime = DateTime.Now.AddDays(1),
+                Description = "my knee hurt",
+                DurationInMin = 15
+            });
+
+            action.Should().NotThrow<Exception>();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void EditValidation_withAppointmentWithNegativeId_ShouldThrowException(int id)
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentId = id,
+                AppointmentDateTime = DateTime.Now.AddDays(1),
+                Description = "my knee hurt",
+                DurationInMin = 15
+            });
+
+            action.Should().Throw<ArgumentException>().WithMessage("Id cannot be negative");
+        }
+
+        [Fact]
+        public void EditValidation_AppointmentWithNoDate_ShouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                Description = "my knee hurt",
+                DurationInMin = 15
+            });
+            action.Should().Throw<ArgumentException>().WithMessage("an appointment needs a dateTime");
+        }
+
+        [Fact]
+        public void EditValidation_AppointmentWithNoDuration_ShouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(1),
+                Description = "my knee hurt"
+            });
+            action.Should().Throw<ArgumentException>().WithMessage("an appointment needs a duration");
+        }
+
+        [Fact]
+        public void EditValidation_AppointmentHasToLongDescription_shouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(1),
+                Description =
+                    "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello" +
+                    "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello" +
+                    "hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohelloh",
+                DurationInMin = 15
+            });
+            action.Should().Throw<ArgumentException>().WithMessage("description is to long");
+        }
+
+        [Fact]
+        public void EditValidation_AppointmentExpiredDate_shouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(-1),
+                Description = "my knee hurt",
+                DurationInMin = 15
+            });
             action.Should().Throw<ArgumentException>().WithMessage("The date it invalid, you cant set an appointment in the past");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void EditValidation_AppointmentNegativeDuration_shouldThrowException(int duration)
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(1),
+                Description = "my knee hurt",
+                DurationInMin = 0
+            });
+            action.Should().Throw<ArgumentException>().WithMessage("The duration cannot be negative");
+        }
+
+        [Fact]
+        public void EditValidation_AppointmentTooLongDuration_shouldThrowException()
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.EditValidation(new Appointment()
+            {
+                AppointmentDateTime = DateTime.Now.AddDays(-1),
+                Description = "my knee hurt",
+                DurationInMin = 1441
+            });
+            action.Should().Throw<ArgumentException>().WithMessage("The duration cannot be longer than one day");
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void IdValidation_withAppointmentWithNegativeId_ShouldThrowException(int id)
+        {
+            IAppointmentValidator appointmentValidator = new AppointmentValidator();
+            Action action = () => appointmentValidator.IdValidation(id);
+
+            action.Should().Throw<ArgumentException>().WithMessage("Id cannot be negative");
         }
 
 
